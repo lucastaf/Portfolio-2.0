7c28e263a2d252e13f6d9b5d56f86000d7232c00
lucas bittencourt rauch <81450319+lucastaf@users.noreply.github.com>
2026-07-26 19:47:17 -0300

    feat: alinhado subtitulos dos projetos

--- components/sections/projects/projects.tsx
+++ components/sections/projects/projects.tsx
@@ -1,7 +1,8 @@
 import { Code, Gamepad2, Monitor } from "lucide-react";
-import { Ref } from "react";
+import { ReactElement, Ref } from "react";
 import { useTranslation } from "react-i18next";
 import ProjectsList from "./projects-list";
+import { projectType } from "@/lib/db/db-types";
 
 export default function Projects(props: { ref: Ref<HTMLDivElement> }) {
   const { ref } = props;
@@ -9,29 +10,42 @@ export default function Projects(props: { ref: Ref<HTMLDivElement> }) {
   return (
     <div className="mt-32 scroll-mt-32" ref={ref}>
       <h1 className="mb-4 ml-16 text-4xl">{t("sections.projects")}</h1>
-      <div>
-        <h1 className="ml-16 text-center text-2xl flex justify-center items-center gap-4 mb-4">
-          <Monitor className="size-8" /> {t("sections.web")}
-        </h1>
-        <div className="mx-16 mb-4 grid grid-cols-2 gap-8">
-          <ProjectsList type="web" />
-        </div>
+      <ProjectsSection
+        title={t("sections.web")}
+        icon={<Monitor className="size-8" />}
+        type="web"
+      />
+      <div className="my-32" />
+      <ProjectsSection
+        title={t("sections.games")}
+        icon={<Gamepad2 className="size-8" />}
+        type="game"
+      />
+      <div className="my-32" />
+      <ProjectsSection
+        title={t("sections.others")}
+        icon={<Code className="size-8" />}
+        type="other"
+      />
+    </div>
+  );
+}
+
+function ProjectsSection(props: {
+  title: string;
+  icon: ReactElement;
+  type: projectType;
+}) {
+  const { icon, title, type } = props;
+  return (
+    <div>
+      <div className="grid grid-cols-[1fr_auto_1fr] gap-2 items-center mb-4">
+        <div className="justify-self-end mr-2">{icon}</div>
+        <h1 className="text-2xl">{title}</h1>
+        <div />
       </div>
-      <div className="mt-32">
-        <h1 className="ml-16 text-center text-2xl flex justify-center items-center gap-4 mb-4">
-          <Gamepad2 className="size-8" /> {t("sections.games")}
-        </h1>
-        <div className="mx-16 mb-4 grid grid-cols-2 gap-8">
-          <ProjectsList type="game" />
-        </div>
-      </div>
-      <div className="mt-32">
-        <h1 className="ml-16 text-center text-2xl flex justify-center items-center gap-4 mb-4">
-          <Code className="size-8" /> {t("sections.others")}
-        </h1>
-        <div className="mx-16 mb-4 grid grid-cols-2 gap-8">
-          <ProjectsList type="other" />
-        </div>
+      <div className="mx-16 mb-4 grid grid-cols-2 gap-8">
+        <ProjectsList type={type} />
       </div>
     </div>
   );
